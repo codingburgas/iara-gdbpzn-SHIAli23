ab5039457d4b0b9b91bdb42846d9340763aac9f3
SHIAli23 <SHIAli23@codingburgas.bg>
2026-03-12 18:05:32 +0200

    Switched backend from SQLite to SQL Server

--- backend/app.py
+++ backend/app.py
@@ -4,12 +4,17 @@ import hashlib
 
 app = Flask(__name__)
 
-# Database config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    "mssql+pyodbc://SHENIZ\\SQLEXPRESS01/GDBPZNData"
+    "?driver=ODBC+Driver+18+for+SQL+Server"
+    "&trusted_connection=yes"
+    "&Encrypt=no"
+    "&TrustServerCertificate=yes"
+)
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,17 +24,12 @@ class User(db.Model):
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
 
-
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def index():
-    return 'Hello World!'
-
-@app.route('/user')
-def user():
-    return 'Hello User!'
+    return 'Hello World from SQL Server!'
 
 if __name__ == '__main__':
     app.run(debug=True)
